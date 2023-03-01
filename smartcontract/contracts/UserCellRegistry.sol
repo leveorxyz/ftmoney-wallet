51d30d798cbe9bac265db676b981fb9d448941a4
rayedsikder <rayedsikder33@gmail.com>
2023-03-02 01:43:20 +0600

    Add state variables

--- smartcontract/contracts/UserCellRegistry.sol
+++ smartcontract/contracts/UserCellRegistry.sol
@@ -4,5 +4,13 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract UserCellRegistry is Ownable {
+    struct Record {
+        address userAddress;
+        bool initialized;
+        string saltHint;
+    }
+
+    mapping(bytes32 => Record) _records;
+    mapping(uint256 => string) _saltHint;
 
 }
