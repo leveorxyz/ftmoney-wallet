381e4b98651732771da5405e56fa456a35f6b026
rayedsikder <rayedsikder33@gmail.com>
2023-03-02 01:44:05 +0600

    Add view function to return user address

--- smartcontract/contracts/UserCellRegistry.sol
+++ smartcontract/contracts/UserCellRegistry.sol
@@ -13,6 +13,20 @@ contract UserCellRegistry is Ownable {
     mapping(bytes32 => Record) _records;
     mapping(uint256 => string) _saltHint;
 
+
+    function getUserAddress(
+        string calldata salt,
+        uint256 userCell
+    ) public view returns (bool isRegistered, address key) {
+        Record memory record = _records[
+            keccak256(abi.encodePacked(salt, userCell))
+        ];
+        if (record.initialized) {
+            isRegistered = true;
+            key = record.userAddress;
+        }
+    }
+
     function registerUser(
         string calldata salt,
         string calldata saltHint,
