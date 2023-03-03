560d7949b351c2abe12d0f107ee27fd48f17dc29
Tahlil <bsse0803@iit.du.ac.bd>
2023-03-04 00:24:08 +0600

    Add ui to enter phone number

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,19 +3,26 @@ import React from "react";
 
 const HomePage: NextPage = () => {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style={{backgroundImage:"url('https://images.unsplash.com/photo-1598798710023-8682deb58a8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80')"}}>
+    <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+      <div className="text-white">
+        <div className="mb-8 flex flex-col items-center">
+          <img src="./logo.png" width="250" alt="" />
+  
+          <span className="text-gray-300">Enter Phone Number for verification</span>
+        </div>
+        <form action="#">
+          <div className="mb-4 text-lg">
+            <input className="rounded-3xl border-none bg-blue-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-100 shadow-lg outline-none backdrop-blur-md" type="text" name="name" placeholder="xxxxxxxx" />
+          </div>
 
-
-      
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Phone number</label>
-          <div className="flex">
-          <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-              @
-          </span>
-       
-          <input type="text" id="website-admin" className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="elonmusk"/>
+          <div className="mt-8 flex justify-center text-lg text-black">
+            <button type="submit" className="rounded-3xl bg-blue-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-blue-600">Enter </button>
+          </div>
+        </form>
       </div>
-     </div>
+    </div>
+  </div>
   );
 };
 
