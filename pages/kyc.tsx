c95147181dcacc882bfe1c554002226a3845ed4d
Tahlil <bsse0803@iit.du.ac.bd>
2023-03-08 10:12:05 +0600

    Add kyc page

--- pages/kyc.tsx
+++ pages/kyc.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { SetStateAction, useState } from 'react';
+import { Switch } from '@headlessui/react';
+import Datepicker from "react-tailwindcss-datepicker";
+
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -7,6 +9,21 @@ function classNames(...classes: string[]) {
 
 export default function Example() {
   const [agreed, setAgreed] = useState(false)
+  const [fName, setFName] = useState("")
+  const [lName, setLName] = useState("")
+  const [email, setEmail] = useState("")
+  const [company, setCompany] = useState("")
+  const [value, setValue] = useState({})
+
+  const handleValueChange = (newValue: SetStateAction<{}>) => {
+    console.log("newValue:", newValue);
+    setValue(newValue);
+  }
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+
+  }
 
   return (
     <div className="isolate bg-white py-24 px-6 sm:py-32 lg:px-8">
@@ -38,7 +55,7 @@ export default function Example() {
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">KYC Form</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+        Streamline compliance with confidence: KYC that meets GDPR standards.
         </p>
       </div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
@@ -54,6 +71,7 @@ export default function Example() {
                 id="first-name"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={e => { setFName(e.currentTarget.value); }}
               />
             </div>
           </div>
@@ -68,7 +86,8 @@ export default function Example() {
                 id="last-name"
                 autoComplete="family-name"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+                onChange={e => { setLName(e.currentTarget.value); }}
+             />
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -82,7 +101,9 @@ export default function Example() {
                 id="company"
                 autoComplete="organization"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+                onChange={e => { setCompany(e.currentTarget.value); }}
+
+            />
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -96,9 +117,27 @@ export default function Example() {
                 id="email"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+                onChange={e => { setEmail(e.currentTarget.value); }}
+            />
+            </div>
+   
+          </div>
+
+          <div className="sm:col-span-2">
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+              Birth Date
+            </label>
+            <div className="mt-2.5">
+            <Datepicker
+                primaryColor={"blue"} 
+                value={value}
+                asSingle={true} 
+                useRange={false} 
+                onChange={handleValueChange}
+            />
             </div>
           </div>
+
           
       
           <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
@@ -135,7 +174,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Submit
           </button>
         </div>
       </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {  
   content: [   
         "./pages/**/*.{js,ts,jsx,tsx}",   
-        "./components/**/*.{js,ts,jsx,tsx}",  
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
          ],
   theme: {   
          extend: {},
          },  
   plugins: [
-       require('@tailwindcss/forms'),
+       require('@tailwindcss/forms')
   ],
 }
