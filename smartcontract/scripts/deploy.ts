540709e087111e152104dba83b667ff87d7f4d31
Tahlil <bsse0803@iit.du.ac.bd>
2023-03-08 11:28:49 +0600

    Deploy cell registry contract

--- smartcontract/scripts/deploy.ts
+++ smartcontract/scripts/deploy.ts
@@ -1,12 +1,24 @@
 import { ethers } from "hardhat";
 import { UserCellRegistry } from "../typechain-types";
+import { writeFile } from "fs/promises";
+
 
 async function main() {
 
     const UserCellRegistryFactory = await ethers.getContractFactory("UserCellRegistry");
     const userCellRegistry: UserCellRegistry = await UserCellRegistryFactory.deploy();
 
-    console.log(`UserCellRegistry deployed to ${userCellRegistry.address}`);
+    const deployedAddress = userCellRegistry.address;
+    console.log(`UserCellRegistry deployed to ${deployedAddress}`);
+
+
+    await writeFile(
+        "../data.json",
+        JSON.stringify({
+          cellRegistryAddress: deployedAddress,
+          cellRegistryLink: "https://testnet.ftmscan.com/address/" + deployedAddress
+        }, null, 2)
+      );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
