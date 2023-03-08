1d29206b36049fe43c3f5157959e810421a268d1
Tahlil <bsse0803@iit.du.ac.bd>
2023-03-08 21:58:12 +0600

    Add signing of KYC verifier

--- pages/api/verifyKYC.ts
+++ pages/api/verifyKYC.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { ethers } from "ethers";
-import abi from "../../abis/UserCellRegistryABI.json";
-import contractDetails from "../../data.json";
-import Web3 from 'web3';
-
+import abi from "../../abis/KYC.json";
+import contractDetails from "../../data2.json";
+import Web3 from "web3";
+import * as EthJS from "ethereumjs-util";
 
 export default async function sendMessage(
   req: NextApiRequest,
@@ -14,22 +14,44 @@ export default async function sendMessage(
   const web3 = new Web3();
 
   const contractABI = abi.abi;
-  const contractAddress = contractDetails.cellRegistryAddress;
-  const { salt, saltHint, userCell, userAddress } = req.body;
+  const contractAddress = contractDetails.kycAddress;
+  const { userAddress, kycHash } = req.body;
+  console.log(privateKey);
+
+  var checksumAddress = web3.utils.toChecksumAddress(userAddress);
+  var payload = web3.utils.sha3(checksumAddress);
+  var { v, r, s } = EthJS.ecsign(
+    EthJS.toBuffer(payload),
+    EthJS.toBuffer("0x" + privateKey)
+  );
+  var rHex = EthJS.bufferToHex(r);
+  var sHex = EthJS.bufferToHex(s);
+  var v = Number(v);
+
+  console.log("calling...");
+  
 
   const signer = new ethers.Wallet(
     privateKey,
-    new ethers.providers.JsonRpcProvider(`https://ftm.getblock.io/${API_TOKEN}/testnet/`)
+    new ethers.providers.JsonRpcProvider(
+      `https://ftm.getblock.io/${API_TOKEN}/testnet/`
+    )
   );
   const contract = new ethers.Contract(contractAddress, contractABI, signer);
-  console.log(salt, saltHint, userCell, userAddress);
-  
-  const tx = await contract.registerUser(salt, saltHint, userCell, userAddress);
+
+  const tx = await contract.approveKYC(
+    checksumAddress,
+    v,
+    rHex,
+    sHex,
+    kycHash,
+    userAddress
+  );
 
   const txReceipt = await tx.wait();
   console.log(txReceipt);
 
   res.status(200).json({
-    "txHash": txReceipt.transactionHash,
+    txHash: txReceipt.transactionHash,
   });
 }
